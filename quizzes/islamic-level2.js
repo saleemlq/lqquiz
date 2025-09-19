75fecf451dfbcbedf6ab69e4e2ab249067f87157
saleemlq <mkhalil654566@gmail.com>
2025-09-19 20:37:52 +0500

    Update islamic-level2.js

--- quizzes/islamic-level2.js
+++ quizzes/islamic-level2.js
@@ -4,13 +4,13 @@ quizzes.push(
     level: 2, quiz: 1,
     questions: [
       { question: "What city did Prophet Muhammad migrate to?", options: ["Makkah", "Medina", "Jerusalem", "Taif"], answer: "Medina" },
-      { question: "What was the first revelation of the Quran?", options: ["Iqra", "Al-Fatiha", "Ya-Sin", "Ikhlas"], answer: "Iqra" },
+      { question: "What was the first revelation of the Quran?", options: ["Al-Alaq", "Al-Fatiha", "Ya-Sin", "Ikhlas"], answer: "Al-Alaq" },
       { question: "Who was the first martyr in Islam?", options: ["Bilal", "Sumayyah", "Umar", "Ali"], answer: "Sumayyah" },
       { question: "Where was the Battle of Badr?", options: ["Badr", "Uhud", "Khandaq", "Khaybar"], answer: "Badr" },
       { question: "Who was the mother of Prophet Isa (A.S)?", options: ["Aisha", "Khadijah", "Maryam", "Fatimah"], answer: "Maryam" },
       { question: "Which Prophet is associated with the Kaaba building?", options: ["Adam", "Ibrahim", "Muhammad", "Nuh"], answer: "Ibrahim" },
       { question: "Prophet Musa received which commandment?", options: ["Ten", "Seven", "Four", "Twelve"], answer: "Ten" },
-      { question: "Who was Prophet Yusuf (Joseph)’s father?", options: ["Jacob", "Isaac", "Ibrahim", "Musa"], answer: "Jacob" },
+      { question: "Who was Prophet Yusuf (Joseph)’s father?", options: ["Yaqoob AS", "Isaac AS", "Ibrahim AS", "Musa AS"], answer: "Yaqoob AS" },
       { question: "Which Prophet spoke in the lap of his mother as a baby?", options: ["Isa", "Nuh", "Adam", "Hud"], answer: "Isa" },
       { question: "Which tribe helped rebuild Kaaba before Islam?", options: ["Quraysh", "Aws", "Thaqif", "Harith"], answer: "Quraysh" }
     ]
