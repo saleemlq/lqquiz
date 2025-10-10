0c8cb9e4b89b9505ccc05fcd4a3f09f401eb77cf
aneeblq <aneebakhtar2@gmail.com>
2025-10-10 23:33:11 +0500

    Update islamic-level3.js

--- quizzes/islamic-level3.js
+++ quizzes/islamic-level3.js
@@ -6,13 +6,13 @@ quizzes.push(
       { question: "Which surah is Meccan but middle of Quran?", options: ["Ya-Sin", "Mulk", "Fatiha", "Rahman"], answer: "Ya-Sin" },
       { question: "How many juz in Quran?", options: ["30", "20", "15", "40"], answer: "30" },
       { question: "How many rukus in Al-Baqarah?", options: ["40", "50", "60", "37"], answer: "40" },
-      { question: "Which surah starts with Mu‑Mir?", options: ["Fussilat", "Shura", "Zukhruf", "Sad"], answer: "Fussilat" },
-      { question: "Which surah ends with 'Salaamun Qawlam Mir Rabbir Rahim'?", options: ["Fussilat", "Shura", "Mulk", "Haqqah"], answer: "Fussilat" },
-      { question: "Which surah is longest in Madani?" ,options: ["Al-Baqarah", "Al-Imran", "An-Nisa", "Al-Ma'idah"], answer: "Al-Baqarah" },
-      { question: "What is the meaning of 'Rahman'?", options: ["Merciful", "Patient", "Just", "Generous"], answer: "Merciful" },
+      { question: "Where was the first revelation revealed?", options: ["Cave Thawr", "Cave Hira", "Cave Noor", "Cave Sina"], answer: "Cave Hira" },
+      { question: "Which Surah does not begin with “Bismillah”?", options: ["Surah Al-Fatiha", "Surah At-Tawbah", "Surah Al-Mulk", "Surah Al-Kahf"], answer: "Surah At-Tawbah" },
+      { question: "Which Surah is called “The Mother of the Quran”?" ,options: ["Surah Al-Ikhlas", "Surah Al-Fatiha", "Surah Al-Ma’idah", "Surah Al-Kawthar"], answer: "Surah Al-Fatiha" },
+      { question: "Which Surah is entirely about women?", options: ["Surah Al-Imran", "Surah An-Nisa", "Surah Maryam", "Surah Al-Hujurat"], answer: "Surah An-Nisa" },
       { question: "Which surah is called Heart of Quran", options: ["Ya-Sin", "Ikhlas", "Fatiha", "Mulk"], answer: "Ya-Sin" },
       { question: "Which surah describes night journey of Prophet?", options: ["Isra", "Mi’raj", "Hijr", "Maryam"], answer: "Isra" },
-      { question: "Which surah opens with 'Kun Fayakun' phrase?", options: ["Ya-Sin", "Maryam", "Sad", "Rahman"], answer: "Sad" }
+      { question: "How many times is the name “Muhammad” mentioned in the Quran?", options: ["2", "20", "4", "28"], answer: "4" }
     ]
   },
   {
